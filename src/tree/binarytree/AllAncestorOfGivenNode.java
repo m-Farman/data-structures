c8cbcaab2d9415e71f46f985a9faa1ed7de9e446
m-Farman <farmanmohd8@gmail.com>
2018-12-20 23:33:43 +0530

    All Ancestor Of Given Node

--- src/tree/binarytree/AllAncestorOfGivenNode.java
+++ src/tree/binarytree/AllAncestorOfGivenNode.java
@@ -10,7 +10,10 @@ public class AllAncestorOfGivenNode {
 	public static void main(String[] args) {
 		TreeNode<Integer> root = TreeUtil.createTreeOfSize(7);
 		ArrayList<Integer> arrayList = new ArrayList<>();
+		System.out.println("Parent to node path : ");
 		getAllAncestors(root, 7, arrayList);
+		System.out.println("Node ancestors ");
+		getAllAncestor(root, 7);
 	}
 
 	private static void getAllAncestors(TreeNode<Integer> root, int node, ArrayList<Integer> path) {
@@ -28,4 +31,25 @@ public class AllAncestorOfGivenNode {
 		path.remove(root.getData());
 	}
 
+	private static boolean getAllAncestor(TreeNode<Integer> root, int node) {
+
+		if (root == null) {
+			return false;
+		}
+		if ((root.getLeft() != null && (Integer) root.getLeft().getData() == node)
+				|| (root.getRight() != null && (Integer) root.getRight().getData() == node)
+				|| getAllAncestor(root.getLeft(), node) || getAllAncestor(root.getRight(), node)) {
+			System.out.print(root.getData() + " ");
+			return true;
+		}
+		return false;
+	}
 }
+
+/*
+output
+Parent to node path : 
+[1, 3, 7]
+Node ancestors 
+3 1 
+*/
