97fe2693ea336dd8e7a9f5109736acd4e4f811dc
m-Farman <farmanmohd8@gmail.com>
2018-12-29 00:31:11 +0530

    Delete Node In Bst

---
+++ src/tree/binarytree/DeleteNodeInBst.java
@@ -0,0 +1,44 @@
+package tree.binarytree;
+
+import tree.TreeNode;
+import tree.TreeUtil;
+
+public class DeleteNodeInBst {
+
+	public static void main(String[] args) {
+		TreeNode<Integer> root = TreeUtil.createBstFromInput(4, 7, 3, 5, 1, 19);
+		System.out.println(root);
+		root = delete(root, 4);
+		System.out.println(root);
+	}
+
+	private static TreeNode<Integer> delete(TreeNode<Integer> root, int node) {
+
+		if (root == null) {
+			return null;
+		}
+
+		if (root.getData() > node) {
+			root.setLeft(delete(root.getLeft(), node));
+		} else if (root.getData() < node) {
+			root.setRight(delete(root.getRight(), node));
+		} else {
+
+			if (root.getLeft() == null) {
+				return root.getRight();
+			} else if (root.getRight() == null) {
+				return root.getLeft();
+			} else {
+				TreeNode<Integer> min = root.getRight();
+				while (min.getLeft() != null) {
+					min = min.getLeft();
+				}
+
+				root.setData(min.getData());
+				root.setRight(delete(root.getRight(), min.getData()));
+			}
+
+		}
+		return root;
+	}
+}
